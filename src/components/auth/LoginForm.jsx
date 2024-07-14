f28e27e377ac95e87bf3b8ee69bf4204f9904b54
JahidulSany <jahidulsanypro@gmail.com>
2024-07-14 21:02:29 +0100

    Login Page

---
+++ src/components/auth/LoginForm.jsx
@@ -0,0 +1,62 @@
+import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
+import Field from "../common/Field";
+
+const LoginForm = () => {
+  const navigate = useNavigate();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const submitForm = (formData) => {
+    console.log(formData);
+    navigate("/");
+  };
+
+  return (
+    <form
+      onSubmit={handleSubmit(submitForm)}
+      className="border-b border-[#3F3F3F] pb-10 lg:pb-[60px]"
+    >
+      <Field label="Email" error={errors.email}>
+        <input
+          {...register("email", { required: "Email is required!" })}
+          className={`auth-input ${
+            errors.email ? "border-red-500" : "border-gray-200"
+          }`}
+          type="email"
+          id="'email"
+          name="email"
+        />
+      </Field>
+      <Field label="Password" error={errors.password}>
+        <input
+          {...register("password", {
+            required: "Password is required",
+            minLength: {
+              value: 8,
+              message: "Password must be at least 8 characters long",
+            },
+          })}
+          className={`auth-input ${
+            errors.password ? "border-red-500" : "border-gray-200"
+          }`}
+          type="password"
+          id="'password"
+          name="password"
+        />
+      </Field>
+      <Field>
+        <button
+          className="auth-input bg-jsGreen font-bold text-deepDark transition-all hover:opacity-90"
+          type="submit"
+        >
+          Login
+        </button>
+      </Field>
+    </form>
+  );
+};
+export default LoginForm;
--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,5 +1,46 @@
+import { Link } from "react-router-dom";
+import AuthIllustration from "../assets/images/auth_illustration.png";
+import LoginForm from "../components/auth/LoginForm";
+
 const LoginPage = () => {
-  return <div>Login Page</div>;
+  return (
+    <main className="flex-center min-h-screen bg-deepDark py-8">
+      <div className="max-w-[1368px] flex-1">
+        <div className="container grid items-center gap-8 lg:grid-cols-2">
+          <div>
+            <img
+              className="mb-12 max-w-full max-lg:hidden"
+              src={AuthIllustration}
+              alt="auth_illustration"
+            />
+            <div>
+              <h1 className="mb-3 text-4xl font-bold lg:text-[40px]">
+                Facehook
+              </h1>
+              <p className="max-w-[452px] text-gray-400/95 lg:text-lg">
+                A social media app with features such as like, showing the post,
+                post details, reactions, comments, profile, and more.
+              </p>
+            </div>
+          </div>
+          <div className="card">
+            <LoginForm />
+            <div className="py-4 lg:py-6">
+              <p className="text-center text-xs text-gray-400/95 lg:text-sm">
+                Don’t have account?&nbsp;
+                <Link
+                  className="text-white transition-all hover:text-jsGreen hover:underline"
+                  to="/register"
+                >
+                  Create New
+                </Link>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
 };
 
 export default LoginPage;
